c700ae9d0fcbc7331474a7117cc80fe3e6c615b4
Kostin Oleg <kov@lins.ru>
2024-06-18 17:42:59 +0300

    Вывод списка файлов

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 from config import Config
 from sys import exit
 
+ENCODING = 'utf-8'
 CONFIG_FILE = 'config.ini'
-OUTPUT_FILE = 'output.txt'
+OUTPUT_NAME_FILES = 'output_name_files.txt'
+OUTPUT_COUNT_FILES = 'output_count_files.txt'
 
 
 def scandir(path: str, level: int):
@@ -15,10 +17,11 @@ def scandir(path: str, level: int):
                     sub_level = level - 1
                     dir_path, dir_count = scandir(entry.path, sub_level)
                     if level > 0:
-                        print(f'{dir_path}, {dir_count}', file=open(OUTPUT_FILE, mode='a'))
+                        print(f'{dir_path}, {dir_count}', file=open(OUTPUT_COUNT_FILES, mode='a', encoding=ENCODING))
                     count += dir_count
                 elif entry.is_file(
                         follow_symlinks=False) and entry.name.endswith(config.extensionFile):
+                    print(os.path.abspath(entry.path), file=open(OUTPUT_NAME_FILES, mode='a', encoding=ENCODING))
                     count += 1
         return path, count
     except Exception as exc:
@@ -26,17 +29,22 @@ def scandir(path: str, level: int):
         return f'{path} - scan error', 0
 
 
-if __name__ == '__main__':
-    config = Config('config.ini')
-    config.load()
-    if os.path.exists(OUTPUT_FILE):
-        print(f'File "{os.path.abspath(OUTPUT_FILE)}" found and cleared.')
+def found_and_clear_output(output_name: str):
+    if os.path.exists(output_name):
+        print(f'File "{os.path.abspath(output_name)}" found and cleared.')
         try:
-            os.remove(OUTPUT_FILE)
+            os.remove(output_name)
         except Exception as exc:
             print(f"{exc}")
             input()
             exit()
+
+
+if __name__ == '__main__':
+    config = Config('config.ini')
+    config.load()
+    found_and_clear_output(OUTPUT_NAME_FILES)
+    found_and_clear_output(OUTPUT_COUNT_FILES)
     if not os.path.exists(CONFIG_FILE):
         print(
             f'File "{CONFIG_FILE}" not found, modify the generated config file ("{os.path.abspath(CONFIG_FILE)}").'
@@ -47,6 +55,6 @@ if __name__ == '__main__':
         exit()
     print('Started count files... ')
     path_dir, count_dir = scandir(config.pathFolder, config.levelDir)
-    print(f'{path_dir}, {count_dir}', file=open(OUTPUT_FILE, mode='a'))
-    print(f'Finished counting files ("{os.path.abspath(OUTPUT_FILE)}"), press any button to exit.')
+    print(f'{path_dir}, {count_dir}', file=open(OUTPUT_COUNT_FILES, mode=ENCODING))
+    print(f'Finished counting files ("{os.path.abspath(OUTPUT_COUNT_FILES)}"), press any button to exit.')
     input()
